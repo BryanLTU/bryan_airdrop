93dc519d7cd31e22e1f4173c092aaa59b2b88815
unknown <thearnas4@gmail.com>
2023-11-17 18:44:17 +0200

    [Update] QBCore compatibility

--- server/functions.lua
+++ server/functions.lua
@@ -1,3 +1,11 @@
+FrameworkObj = nil
+
+if Config.Framework == 'esx' then
+    FrameworkObj = exports['es_extended']:getSharedObject()
+elseif Config.Framework == 'qbcore' then
+    FrameworkObj = exports['qb-core']:GetCoreObject()
+end
+
 _GetClosestPlayer = function(coords)
     local closestPlayer, closestDistance = -1, -1
 
@@ -14,25 +22,46 @@ _GetClosestPlayer = function(coords)
 end
 
 _AddPlayerItem = function(source, item, amount)
-    ESX.GetPlayerFromId(source).addInventoryItem(item, amount)
+    if Config.Framework == 'esx' then
+        FrameworkObj.GetPlayerFromId(source).addInventoryItem(item, amount)
+    elseif Config.Framework == 'qbcore' then
+        FrameworkObj.Functions.GetPlayer(source).Functions.AddItem(item, amount)
+    end
 end
 
 _AddPlayerWeapon = function(source, weapon)
-    ESX.GetPlayerFromId(source).addWeapon(weapon, 1)
+    if Config.Framework == 'esx' then
+        FrameworkObj.GetPlayerFromId(source).addWeapon(weapon, 1)
+    end
 end
 
 _AddPlayerMoney = function(source, account, amount)
-    ESX.GetPlayerFromId(source).addAccountMoney(account, amount)
+    if Config.Framework == 'esx' then
+        FrameworkObj.GetPlayerFromId(source).addAccountMoney(account, amount)
+    elseif Config.Framework == 'qbcore' then
+        FrameworkObj.Functions.GetPlayer(source).Functions.AddMoney(account, amount)
+    end
 end
 
 _AddPlayerVehicle = function(source, model, plate)
     if Config.Framework == 'esx' then
         MySQL.insert('INSERT INTO owned_vehicles (owner, plate, vehicle) VALUES (?, ?, ?)', {
-            ESX.GetPlayerFromId(source).getIdentifier(),
+            FrameworkObj.GetPlayerFromId(source).getIdentifier(),
             plate,
             json.encode({ model = joaat(model), plate = plate })
         })
     elseif Config.Framework == 'qbcore' then
-        
+        local pData = FrameworkObj.Functions.GetPlayer(source)
+
+        MySQL.insert('INSERT INTO player_vehicles (license, citizenid, vehicle, hash, mods, plate, garage, state) VALUES (?, ?, ?, ?, ?, ?, ?, ?)', {
+            pData.PlayerData.license,
+            pData.PlayerData.citizenid,
+            model,
+            GetHashKey(vehicle),
+            '{}',
+            plate,
+            'pillboxgarage',
+            0
+        })
     end
 end
